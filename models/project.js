f5666d44851a4a59299ad07a781d7600a738ab44
Abhishek447-ai <abhishekokali100@gmail.com>
2026-07-02 16:24:53 +0530

    nn

--- models/project.js
+++ models/project.js
@@ -1,16 +1,35 @@
 const mongoose = require("mongoose");
 
-const projectSchema = new mongoose.Schema({
-  fullName: String,
-  mobile: String,
-  email: String,
-  projectTitle: String,
-  language: String,
-  database: String,
-  createdAt: {
-    type: Date,
-    default: Date.now,
+const projectSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    projectTitle: {
+      type: String,
+      required: true,
+    },
+    language: {
+      type: String,
+      required: true,
+    },
+    database: {
+      type: String,
+      required: true,
+    },
   },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Project", projectSchema);
